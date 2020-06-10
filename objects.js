73f89ce78d92c5ca436d734c93bf908e6b0586cf
MuathAlawi Alawi <muathalawi1@gmail.com>
2020-06-10 03:44:11 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,5 +5,5 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  return delete removeFromPlaylist.artistName;
+  return delete playlist.artistName;
 }
