32cb6808a80af896b087795b559a81a6e9d85f67
MuathAlawi Alawi <muathalawi1@gmail.com>
2020-06-10 03:43:31 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = {Nas: 'Nas is like',};
 function updatePlaylist(playlist, artistName, songTitle) {
   return playlist[artistName] = songTitle;
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  return delete removeFromPlaylist.artistName;
+}
